4968860e2186d7aadcff7a233293971f6667220b
unknown <nerio.ferrebuscastillo@unicolombo.edu.co>
2023-09-09 18:11:23 -0500

    Graficos

---
+++ Bancografico/src/main/java/com/mycompany/bancografico/Bancografico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancografico;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Bancografico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
